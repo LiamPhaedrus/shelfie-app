5d538da225933f6aea66c37ba79ce281e9a3e3a9
LiamPhaedrus <liamphaedrus@gmail.com>
2017-05-12 00:57:16 -0400

    more styling

---
+++ react/src/components/BookListTile.js
@@ -0,0 +1,35 @@
+import React, { Component }  from 'react'
+import { Link } from 'react-router'
+
+const BookListTile = (props) => {
+  let showAuthor = ''
+  if (props.author !== '') {
+    showAuthor = <span>{'By ' + props.author}</span>
+  }
+  let showImage = ''
+  if (props.coverPhoto) {
+    showImage = <img src={props.coverPhoto} alt={`${props.title}`} />
+  }
+  return(
+    <div className='media-object book-list-tile'>
+      <div className='media-object-section'>
+        <div className='thumbnail'>
+          {showImage}
+        </div>
+      </div>
+      <div className='media-object-section'>
+        <p><span className='title-strong'>{props.title}</span> {showAuthor}</p>
+      </div>
+    </div>
+  )
+}
+
+export default BookListTile
+
+
+
+// <div className='book-list-outer columns'>
+// <Link to={`/books/${props.id}`} className='book-list-tile'>
+// {showImage}<span className='title-strong'>{props.title}</span> {showAuthor}
+// </Link>
+// </div>
--- react/src/containers/BooksContainer.js
+++ react/src/containers/BooksContainer.js
@@ -1,7 +1,7 @@
 import React, { Component }  from 'react'
 import { Link } from 'react-router'
-import BackButton from '../components/BackButton'
 import NewBookFormContainer from './NewBookFormContainer'
+import BookListTile from '../components/BookListTile'
 
 class BooksContainer extends Component {
   constructor (props) {
@@ -26,26 +26,38 @@ class BooksContainer extends Component {
 
   render () {
     let bookList = this.state.books.map(book => {
-      let author = ''
-      if (book.author) {
-        author = `by ${book.author}`
-      }
       return(
-        <li key={"book" + book.id}><Link to={`/books/${book.id}`} className="book-link">
-          <span className='title-strong'>{book.title}</span> {author}</Link>
-        </li>
+        <BookListTile
+          key={'book' + book.id}
+          id={book.id}
+          title={book.title}
+          author={book.author}
+          isbn={book.isbn}
+          coverPhoto={book.cover_photo}
+          selectedBookId={this.state.selectedBookId}
+        />
       )
+      // let author = ''
+      // if (book.author) {
+      //   author = `by ${book.author}`
+      // }
+      // return(
+      //   <li key={"book" + book.id}><Link to={`/books/${book.id}`} className="book-link">
+      //     <span className='title-strong'>{book.title}</span> {author}</Link>
+      //   </li>
+      // )
     })
     return(
       <div className='bg-fade'>
         <div className='columns'>
           <h1>Your Books</h1>
           <Link to='books/new' className="button react-left">Add Book</Link>
-          <BackButton />
-          <ul>
+          <Link to='/' className="button">Home</Link>
+          <div className='row'>
             {bookList}
-          </ul>
-          <a href='#top-all-books' className="button react-left">Top</a><BackButton />
+          </div>
+          <a href='#top-all-books' className="button react-left">Top</a>
+          <Link to='/' className="button">Home</Link>
         </div>
       </div>
     )
