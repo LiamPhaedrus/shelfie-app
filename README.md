625c5320d802221de5e2d2b70e9144ff92fb8442
LiamPhaedrus <liamphaedrus@gmail.com>
2017-05-14 15:57:27 -0400

    readme

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 ![Build Status](https://codeship.com/projects/c9a28d60-0f30-0135-f229-3e3b859db2c9/status?branch=master)
 ![Code Climate](https://codeclimate.com/github/LiamPhaedrus/lib-app.png)
-![Coverage Status](https://coveralls.io/repos/LiamPhaedrus/lib-app/badge.png)
+
+#Shelfie
+
+This is my capstone, or 'Breakable Toy', for Launch Academy. It is a web application to help users organize their books at home. For those of us who have a ton of books and don't remember where we put them, or still have books in boxes from three moves ago, Shelfie gives the user the ability to better keep track of where their books are.
+
+#Technology
+
+I integrated the following technologies and features in this project:
+
+* Ruby on Rails back end with a PostgreSQL database
+* React front-end for seamless user experience
+* Drag and drop library implemented in React for ease of organizing books
+* Foundation with custom CSS styling for responsive design
+
+#Future Development
+
+I have a variety of ideas for where this app could go next:
+
+* Incorporate an external API (such as Goodreads) to make it much easier for users to add books
+* Set up a suggested 'sort' function so that the app could sort the books by a variety of features, such as authors, categories, etc.
+* Separate authors out from books so that users could more easily sort their books, especially in the case of implementing a sort function.
--- spec/controllers/api_v1_shelves_spec.rb
+++ spec/controllers/api_v1_shelves_spec.rb
@@ -6,8 +6,6 @@ describe Api::V1::ShelvesController, type: :controller do
   describe "GET #index" do
     before(:each) do
       @user = FactoryGirl.create(:user)
-      # @book = FactoryGirl.create(:book)
-      # @book_two = FactoryGirl.create(:book)
     end
 
     it 'it returns nothing if not signed in' do
@@ -15,62 +13,5 @@ describe Api::V1::ShelvesController, type: :controller do
 
       expect(json_parsed_response).to have_content("you do not have access to this page")
     end
-
-  #   it 'it returns data to a signed in user' do
-  #     Placement.create(user: @user, book: @book)
-  #     Placement.create(user: @user, book: @book_two)
-  #     sign_in(@user)
-  #     get :index
-  #
-  #     expect(response.status).to eq 200
-  #
-  #     expect(json_parsed_response["books"]).to have_content(@book.author)
-  #     expect(json_parsed_response["books"]).to have_content(@book_two.title)
-  #   end
   end
-
-  # describe "GET #show" do
-  #   before(:each) do
-  #     @user = FactoryGirl.create(:user)
-  #     @book = FactoryGirl.create(:book)
-  #     @book_two = FactoryGirl.create(:book)
-  #   end
-  #
-  #   it "it returns only one book's data to a signed in user" do
-  #     Placement.create(user: @user, book: @book)
-  #     Placement.create(user: @user, book: @book_two)
-  #     sign_in(@user)
-  #     get :show, params: { id: @book.id }
-  #
-  #     expect(response.status).to eq 200
-  #
-  #     expect(json_parsed_response).to have_content(@book.title)
-  #     expect(json_parsed_response).to_not have_content(@book_two.title)
-  #   end
-  # end
-  #
-  #
-  # describe "POST #create" do
-  #   let(:user) {create :user}
-  #
-  #   let(:correct_book_params) { { title: 'Feed', author: 'Mira Grant', isbn: '9780316081054' } }
-  #   let(:bad_params) { { title: '', isbn: '10' } }
-  #
-  #   it "user can successfully add a book and gets back JSON" do
-  #     sign_in(user)
-  #     expect { post :create, params: { book: correct_book_params} }.to change{ user.books.count }.by 1
-  #   end
-  #
-  #   it "returns an error when payload is incorrect" do
-  #     sign_in(user)
-  #     post :create, params: { book: bad_params }
-  #
-  #     expect(response.status).to eq 400
-  #     expect(json_parsed_response.keys).to have_content('error')
-  #     expect(json_parsed_response['error']).to have_content({
-  #       "title"=>["can't be blank"],
-  #       "isbn"=>["is too short (minimum is 10 characters)"]
-  #     })
-  #   end
-  # end
 end
